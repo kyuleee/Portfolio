f3160a796ea599fe4b98e510f0d3b1dd354b5c97
kyuleee <mindy7453@naver.com>
2023-12-13 11:19:53 +0900

    1213 작업1차

--- src/page/MainPage/section/Experience.js
+++ src/page/MainPage/section/Experience.js
@@ -1,4 +1,4 @@
-import { OrbitControls,ContactShadows } from "@react-three/drei";
+import { OrbitControls, ContactShadows } from "@react-three/drei";
 import { Avatar } from "./Avator";
 
 export const Experience = () => {
@@ -6,16 +6,15 @@ export const Experience = () => {
         <>
             <OrbitControls />
             <group position-y={-1}>
-            <ContactShadows
-          opacity={0.42}
-          scale={10}
-          blur={1}
-          far={10}
-          resolution={256}
-          color="#000000"
-        />
+                <ContactShadows
+                    opacity={0.42}
+                    scale={10}
+                    blur={1}
+                    far={10}
+                    resolution={256}
+                    color="#000000"
+                />
                 <Avatar />
-
             </group>
             <ambientLight intensity={1} />
         </>
--- src/page/MainPage/section/project.js
+++ src/page/MainPage/section/project.js
@@ -1,7 +1,9 @@
 import './project.css';
 // import { siteText } from '../../../data/index';
 import { forwardRef, useEffect, useRef } from 'react';
-import M from 'materialize-css';
+// import M from 'materialize-css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faPlay } from '@fortawesome/free-solid-svg-icons';
 
 const ProjectSection = forwardRef(function ProjectSection(props, ref) {
     const carouselRef = useRef(null);
@@ -33,84 +35,29 @@ const ProjectSection = forwardRef(function ProjectSection(props, ref) {
     //     M.Carousel.init(carouselRef.current, {});
     //   }, []);
     const projectData = [
-        { title: '광주컨트리클럽', contribute: '100%', period: '3 weeks', useStack: 'HTML5, CSS3, JavsScript', code: 'https://github.com/kyuleee/golf', view: 'https://kyuleee.github.io/golf/', review: '힘듦..', gif: '' },
-        { title: '경복궁 별빛야행', contribute: '40%', period: '4 weeks', useStack: 'HTML5, CSS3, node.js, react', code: 'https://github.com/kyuleee/Gyeongbokgung-Starlight-Tour', view: 'https://kyuleee.github.io/Gyeongbokgung-Starlight-Tour/', review: '팀플망해라', gif: '' },
-        { title: '프론트엔드 사전', contribute: '100%', period: '3days', useStack: 'HTML5, CSS3, node.js, react', code: 'https://github.com/kyuleee/dictionary', view: 'https://kyuleee.github.io/dictionary/', review: '뭐가 뭔지 1도 모름', gif: '' },
-        { title: '독:수리', contribute: '40%', period: '4 weeks', useStack: 'HTML5, CSS3, node.js, react', code: 'https://github.com/kyuleee/docsuri', view: 'https://kyuleee.github.io/docsuri/', review: '팀장 다시는 안 해', gif: '' }
+        { title: '광주컨트리클럽',img:'golf', contribute: '100%', period: '3 weeks', useStack: 'HTML5, CSS3, JavsScript', code: 'https://github.com/kyuleee/golf', view: 'https://kyuleee.github.io/golf/', review: '힘듦..', gif: '' },
+        { title: '경복궁 별빛야행',img:'palace', contribute: '40%', period: '4 weeks', useStack: 'HTML5, CSS3, node.js, react', code: 'https://github.com/kyuleee/Gyeongbokgung-Starlight-Tour', view: 'https://kyuleee.github.io/Gyeongbokgung-Starlight-Tour/', review: '팀플망해라', gif: '' },
+        { title: '프론트엔드 사전',img:'dict', contribute: '100%', period: '3days', useStack: 'HTML5, CSS3, node.js, react', code: 'https://github.com/kyuleee/dictionary', view: 'https://kyuleee.github.io/dictionary/', review: '뭐가 뭔지 1도 모름', gif: '' },
+        { title: '독:수리',img:'dict', contribute: '40%', period: '4 weeks', useStack: 'HTML5, CSS3, node.js, react', code: 'https://github.com/kyuleee/docsuri', view: 'https://kyuleee.github.io/docsuri/', review: '독수리수리수리마수리', gif: '' }
     ];
 
     return (
         <div className="projectSection" id='project' ref={reviewRef => (ref.current[2] = reviewRef)}>
+            <h2>PROJECT</h2>
             <div className='banner'>
-                {/* <div className='content first active'>
-                    <img className='movie-title' />
-                    <h4>
-                        <span>2023</span>
-                        <span>100%</span>
-                        <span>2weeks</span>
-                        <span>gwangju</span>
-                    </h4>
-                    <p>1Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt aliquet egestas. Aliquam facilisis, arcu vel tincidunt facilisis, turpis sapien accumsan neque, ut tempor nibh lacus at mi. Nam laoreet a massa quis ultrices. Etiam eget mi dapibus, vehicula nibh vel, finibus odio. Suspendisse in diam vehicula felis hendrerit molestie. Sed diam erat, pellentesque in enim et, auctor tincidunt urna. Curabitur vitae massa dictum, auctor justo non, gravida neque</p>
-                    <div className='button'>
-                        <a href='#'><i className='fa fa-play' aria-hidden='true'>Watch</i></a>
-                        <a href='#'><i className='fa fa-plus' aria-hidden='true'>My List</i></a>
-                    </div>
-                </div>
-                <div className='content bg-65'>
-                    <img className='movie-title' />
-                    <h4>
-                        <span>2023</span>
-                        <span>100%</span>
-                        <span>32weeks</span>
-                        <span>dokdo</span>
-                    </h4>
-                    <p>2Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt aliquet egestas. Aliquam facilisis, arcu vel tincidunt facilisis, turpis sapien accumsan neque, ut tempor nibh lacus at mi. Nam laoreet a massa quis ultrices. Etiam eget mi dapibus, vehicula nibh vel, finibus odio. Suspendisse in diam vehicula felis hendrerit molestie. Sed diam erat, pellentesque in enim et, auctor tincidunt urna. Curabitur vitae massa dictum, auctor justo non, gravida neque</p>
-                    <div className='button'>
-                        <a href='#'><i className='fa fa-play' aria-hidden='true'>Watch</i></a>
-                        <a href='#'><i className='fa fa-plus' aria-hidden='true'>My List</i></a>
-                    </div>
-                </div>
-                <div className='content blacn'>
-                    <img className='movie-title' />
-                    <h4>
-                        <span>2023</span>
-                        <span>30%</span>
-                        <span>2mon</span>
-                        <span>kolon</span>
-                    </h4>
-                    <p>3Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt aliquet egestas. Aliquam facilisis, arcu vel tincidunt facilisis, turpis sapien accumsan neque, ut tempor nibh lacus at mi. Nam laoreet a massa quis ultrices. Etiam eget mi dapibus, vehicula nibh vel, finibus odio. Suspendisse in diam vehicula felis hendrerit molestie. Sed diam erat, pellentesque in enim et, auctor tincidunt urna. Curabitur vitae massa dictum, auctor justo non, gravida neque</p>
-                    <div className='button'>
-                        <a href='#'><i className='fa fa-play' aria-hidden='true'>Watch</i></a>
-                        <a href='#'><i className='fa fa-plus' aria-hidden='true'>My List</i></a>
-                    </div>
-                </div>
-                <div className='content covenant'>
-                    <img className='movie-title' />
-                    <h4>
-                        <span>2023</span>
-                        <span>40%</span>
-                        <span>3mon</span>
-                        <span>mywokr</span>
-                    </h4>
-                    <p>4Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt aliquet egestas. Aliquam facilisis, arcu vel tincidunt facilisis, turpis sapien accumsan neque, ut tempor nibh lacus at mi. Nam laoreet a massa quis ultrices. Etiam eget mi dapibus, vehicula nibh vel, finibus odio. Suspendisse in diam vehicula felis hendrerit molestie. Sed diam erat, pellentesque in enim et, auctor tincidunt urna. Curabitur vitae massa dictum, auctor justo non, gravida neque</p>
-                    <div className='button'>
-                        <a href='#'><i className='fa fa-play' aria-hidden='true'>Watch</i></a>
-                        <a href='#'><i className='fa fa-plus' aria-hidden='true'>My List</i></a>
-                    </div>
-                </div> */}
                 {projectData.map((project, index) => (
                     <div key={index} className={`content ${project.title} ${index === 0 ? 'active' : ''}`}>
                         <img className="movie-title" />
                         <h4>
-                            <span>{project.title}</span>
+                            <p>{project.title}</p>
                             <span>{project.contribute}</span>
                             <span>{project.period}</span>
                             <span>{project.useStack}</span>
                         </h4>
                         <p>{project.review}</p>
                         <div className='button'>
-                            <a href={project.view} target='_blank' rel='noopener noreferrer'><i className='fa fa-play' aria-hidden='true'>Watch</i></a>
-                            <a href={project.code} target='_blank' rel='noopener noreferrer'><i className='fa fa-plus' aria-hidden='true'>My List</i></a>
+                            <a href={project.view} target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faPlay} pulse/>Watch</a>
+                            <a href={project.code} target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faPlus} spin/>My Code</a>
                         </div>
                     </div>
                 ))}
@@ -134,7 +81,7 @@ const ProjectSection = forwardRef(function ProjectSection(props, ref) {
                     <div className='carousel' ref={carouselRef}>
                         {projectData.map((project, index) => (
                             <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`} onClick={() => changeBg(index, project.title)}>
-                                <img src={`/Portfolio/img/${project.title}.jpg`} alt={project.title} />
+                                <img src={`/Portfolio/img/${project.img}.jpg`} alt={project.title} />
                             </div>
                         ))}
                     </div>
